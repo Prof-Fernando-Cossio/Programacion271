9ba614fe7272b85c3d91d14c74f75df1a80b881b
Nuribeth_Rosales <namvalin@gmail.com>
2026-08-24 12:58:19 -0600

    Code of cube moving
    Still in progress

---
+++ EjercicosProgramación/Assets/Nury_Ros/Scripts/CubeMovement.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class CubeMovement : MonoBehaviour
+{
+    public int speed, distance;
+    Vector3 positionobj;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
