eb2996cc9dce37a899e68a2057ef36bd38a059ec
Nuribeth_Rosales <namvalin@gmail.com>
2026-08-25 23:14:10 -0600

    Activity done
    The cube now moves side to side

--- EjercicosProgramación/Assets/Nury_Ros/Scripts/CubeMovement.cs
+++ EjercicosProgramación/Assets/Nury_Ros/Scripts/CubeMovement.cs
@@ -2,17 +2,33 @@ using UnityEngine;
 
 public class CubeMovement : MonoBehaviour
 {
-    public int speed, distance;
-    Vector3 positionobj;
-    
+    public Transform point_a,point_b;
+    public int speed=2;
+    Transform where_to_go;
+
     void Start()
     {
-        
+        where_to_go=point_a;
     }
-
     
     void Update()
     {
+        transform.position=Vector3.MoveTowards(transform.position , where_to_go.position,speed*Time.deltaTime);
         
+        //Vector3.MoveTowards(posicion actual,posicion de destino,cuanto avanzo)
+        
+        if (Vector3.Distance(transform.position,where_to_go.position) < .1f) 
+        //Vector3.Distance(obtener distancia entre 2 posiciones)
+        {
+            if (where_to_go==point_b)
+            {
+            where_to_go=point_a;
+            }
+            else 
+            {
+                where_to_go=point_b;
+            }
+
+        }
     }
 }
