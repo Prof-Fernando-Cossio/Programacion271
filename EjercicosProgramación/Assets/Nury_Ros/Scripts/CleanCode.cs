0f4fb4650b1e1f45bd7cf10902b39e68de30ffc4
Nuribeth_Rosales <namvalin@gmail.com>
2026-08-27 13:00:04 -0600

    CleanCode

---
+++ EjercicosProgramación/Assets/Nury_Ros/Scripts/CleanCode.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class CleanCode : MonoBehaviour
+{
+    
+    //1
+    [SerializeField] private float _movementSpeed=5f;
+
+    //2
+    public float PlayerLevel;
+
+    //3
+    protected float _baseDamage=10f;
+
+    //4
+    private float _playerLife;
+
+    //5
+    [SerializeField] private Rigidbody _rigidbody;
+
+    //6
+    bool 
+
+
+    
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
