23a0a6f5b521113d65f0e8e1a2e127590f92ba9e
Linkling81 <carlosrg81j@gmail.com>
2026-09-14 11:18:46 -0600

    Exercises

--- EjercicosProgramación/Assets/Assets/Scripts_CR/Exercise_02_CR.cs
+++ EjercicosProgramación/Assets/Assets/Scripts_CR/Exercise_02_CR.cs
@@ -1,16 +1,89 @@
+using NUnit.Framework;
+using System;
+using System.Reflection.Metadata.Ecma335;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Exercise_02_CR : MonoBehaviour
 {
     //1.
-    private void PlayerTakeDamage(int damageAmount, int currentLife)
+    public void PlayerTakeDamage(int damageAmount, int currentLife)
+    {
+        currentLife = currentLife - damageAmount;
+        if(currentLife < 0 ) { currentLife = 0; }
+    }
+
+    //2.
+    public bool IsPlayerAlive(int currentLife)
+    {
+        if (currentLife > 0) { return true; }
+        else { return false; }
+    }
+
+    //3.
+    public float DistanceBetween(Vector3 playerPosition, Vector3 city)
+    {
+        return math.distance(playerPosition, city);
+    }
+
+    //4. 
+    public void NormalizeDistance(Vector3 distanceBetween)
+    {
+        math.normalize(distanceBetween);
+    }
+
+    //5.
+    public string PlayerName(string playerName)
+    {
+        playerName = Console.ReadLine();
+        return playerName;
+    }
+
+
+    //6.
+    public int EnemyList(Array enemyArray)
+    {
+        return enemyArray.Length;
+    }
+
+    //7.
+    public Vector3 NearestEnemy(Vector3 playerPosition, Vector3[] enemyArray)
+    {
+        float nearestDistance = float.MaxValue;
+        Vector3 nearestEnemy = Vector3.zero;
+
+        foreach (Vector3 enemy in enemyArray)
+        {
+            float distance = Vector3.Distance(playerPosition, enemy);
+
+            if (distance < nearestDistance)
+            {
+                nearestDistance = distance;
+                nearestEnemy = enemy;
+            }
+        }
+
+        return nearestEnemy;
+    }
+
+    //8.
+    public void PlayerSpeed(float movementSpeed, Vector3 playerDirection)
+    {
+        transform.position = playerDirection * movementSpeed * Time.deltaTime;
+    }
+
+    //9.
+    public void AnglesConverter(float angle)
+    {
+        float radian = Mathf.Rad2Deg * angle;
+    }
+
+    //10.
+    public bool TryFindPlayer(string playerName, Vector3 position, float maxDistance = 10f, LayerMask layermask = default, out Player foundPlayer, out float distance)
     {
         
     }
 
-    //2.
-    public void IsPlayerAlive(int currentLife) { }
+    public class Player {}
 
-    //3.
-    private void DistanceBetween(GameObject object1, GameObject object2) { }
 }
