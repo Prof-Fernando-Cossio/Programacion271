52458d68030d3ebbfd69054d46e15eb242d0f70a
Nuribeth_Rosales <namvalin@gmail.com>
2026-09-14 12:12:16 -0600

    3 more Debug.Log

--- EjercicosProgramación/Assets/Scripts/MainPlayer.Nicky.cs
+++ EjercicosProgramación/Assets/Scripts/MainPlayer.Nicky.cs
@@ -4,6 +4,12 @@ public partial class MainPlayer : MonoBehaviour
 {
     public void UsingPartialClass()
     {
-    Debug.Log("Hola soy Val");
+        Debug.Log("Hola soy Val");
+        Debug.Log("tengo mucho sueño");
+        Debug.Log("Ya casi salimos de la uni");
+        Debug.Log("Ya quiero llegar a mi casita");
     }
+
+    //Ctrl RR: te permite modificar los nombres de cualquier cosa y lo propaga por todas partes
+    //F12: Para que te lleve directo a donde esta declarado la cosa
 }
